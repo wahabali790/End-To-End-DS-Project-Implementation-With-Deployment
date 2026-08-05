d8ad384921002ec7e83a653398c8b3ca51408c71
wahabali790 <wahabalibb7@gmail.com>
2026-08-05 16:34:26 +0500

    ebf4

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
-application=Flask(__name__)
+app=Flask(__name__)
 
-app=application
+app=app
 
 ## Route for a home page
 
